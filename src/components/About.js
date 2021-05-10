0d3e869ec4d98a9606706b0fe1d0c56dca9606cb
Alberto Polanco <a.p.r.13287@gmail.com>
2021-05-10 17:42:03 +0200

    parte de about

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,7 @@
-import { Card, CardContent, CardMedia, makeStyles, Typography } from '@material-ui/core';
+import { Button, Card, CardActions, CardContent, CardMedia, makeStyles, Typography } from '@material-ui/core';
 import foto from "../images/fotoAlberto.jpg"
+import TypeWriterEffect from "react-typewriter-effect"
+import pdf from "../images/cv.pdf"
 
 const About = ({title, dark, id}) => {
     const classes = useStyles();
@@ -10,8 +12,29 @@ const About = ({title, dark, id}) => {
                 <Card className={ classes.card }>
                     <CardMedia image={ foto } className={ classes.media } title="picture"/>
                     <CardContent className={ classes.cardcontent }>
-                        Contenido de la tarjeta
+                        <TypeWriterEffect 
+                            text= "Hola, soy Alberto Polanco"
+                            textStyle={{fontSize: "2rem", fontWeight: "700px", color: "tomato"}}
+                            startDelay={100}
+                            cursorColor="Black"
+                            typeSpeed={100}
+                        />
+                        <TypeWriterEffect 
+                            text= "y soy Junior Developer"
+                            textStyle={{fontSize: "1.2rem", fontWeight: "500px", }}
+                            startDelay={2500}
+                            cursorColor="Black"
+                            typeSpeed={100}
+                        />
                     </CardContent>
+                    <CardActions>
+                        <Button variant="contained" className={ classes.pdfbutton}>
+                            <a href= {pdf} download>
+                            Download CV
+                            </a>
+                            
+                        </Button>
+                    </CardActions>
                 </Card>
             </div>
         </div>
@@ -33,11 +56,38 @@ const useStyles = makeStyles((theme) => ({
     card: {
         height: "70vh",
         display: "flex",
+        marginTop: theme.spacing(6),
+        position: "relative",
     },
     media: {
        width: "250px",
        height: "auto",
        objectFit: "cover",
+       borderRadius: "10px",
+       margin: theme.spacing(5),
+    },
+    pdfbutton: {
+        position: "absolute",
+        bottom: "5rem",
+        right: "4rem",
+        [theme.breakpoints.down("sm")]: {
+            bottom: 10,
+            right: "1rem",
+        },
+        backgroundColor: "tomato",
+        padding: theme.spacing(3),
+        "&:hover":{
+            backgroundColor: "#fff",
+        },
+        "& a": {
+            color: "#fff",
+            textDecoration: "none",
+            fontWeight: 900,
+
+        },
+        "& a:hover": {
+            color: "tomato"
+        }
     },
   }))
 
